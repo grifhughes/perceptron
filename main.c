cb9be470014b712fd8273ea7f8b531b0d2084ee6
Grif Hughes <ghughes@smu.edu>
2017-01-06 00:56:44 -0600

    tuned parameters

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include "perceptron.h"
 #include <stdio.h>
 
-#define MAX_ITER 50 
-#define LEARN_RATE 0.1
-#define NEXAMPLES 85 
-#define NFEATURES 4 
+#define MAX_ITER 750 
+#define LEARN_RATE 0.15
+#define NEXAMPLES 130 
+#define NFEATURES 13 
 
 int
 main(void)
@@ -13,22 +13,25 @@ main(void)
     struct tset_pair *training_examples[NEXAMPLES]; 
 
     fp = fopen("data.txt", "r");
+    int classes[NEXAMPLES] = {0};
 
     /* parse file */
     for(int i = 0; i < NEXAMPLES; ++i) {
         float tmp[NFEATURES];
-        int class = 0;
-        fscanf(fp, "%f,%f,%f,%f,%d", &tmp[0], &tmp[1], &tmp[2], &tmp[3], &class); 
-        training_examples[i] = build_tset_pair(tmp, class, NFEATURES);
+        fscanf(fp, "%d,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f", &classes[i], &tmp[0], &tmp[1], &tmp[2], &tmp[3], &tmp[4],
+                &tmp[5], &tmp[6], &tmp[7], &tmp[8], &tmp[9], &tmp[10], &tmp[11], &tmp[12]); 
+        training_examples[i] = build_tset_pair(tmp, classes[i], NFEATURES);
     }
 
     struct perceptron *p = build_perceptron(NFEATURES, LEARN_RATE);
     learn(p, training_examples, NEXAMPLES, MAX_ITER);
 
+    float nwrong = 0;
     for(int i = 0; i < NEXAMPLES; ++i) { 
-        printf("class: %s\n", classify(p, training_examples[i]) ? 
-                "Iris-setosa" : "Iris-virginica");
+        if(classes[i] != classify(p, training_examples[i]))
+            nwrong++;
         destroy_tset_pair(training_examples[i]);
     }
+    printf("percent correct: %f\n", 100.0f - ((nwrong / NEXAMPLES) * 100.0f));
     destroy_perceptron(p);
 }
